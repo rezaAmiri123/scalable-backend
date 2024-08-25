dc96d0fbf950d3c5f936d43b6e06f299e77bea9b
Reza Amiri <reza.amiri.ac@gmail.com>
2024-08-25 17:10:59 +0300

    initiate

---
+++ internal/database/gorm.go
@@ -0,0 +1,10 @@
+package database
+
+import (
+	"github.com/samber/lo"
+	"github.com/sirupsen/logrus"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/plugin/dbresolver"
+)
